58359a5cbb1d56a825741a985ad01209f0cadf5f
gasimo <georgios.asimomitis@gmail.com>
2017-09-06 00:29:08 +0300

    Draft Test

--- tests/testthat/test-BM.R
+++ tests/testthat/test-BM.R
@@ -45,35 +45,10 @@ b.Sigmae2 <- rbind(c(.2, 0, 0),
                    c(0, 0, .4))
 
 
-a.OU.Alpha <- rbind(c(EPS, 0, 0),
-                    c(0, EPS, 0),
-                    c(0, 0, EPS))
-a.OU.Theta <- c(0, 0, 0)
-
-# in regime 'b.OU' there is correlation between the traits. They evolve under the OU process.
-Alpha <- abind::abind(a.OU.Alpha, a.OU.Alpha, along=-1, new.names=list(regime=c('a','a.OU'), x=NULL, y=NULL))
-Theta <- abind::abind(a.OU.Theta, a.OU.Theta, along=-1, new.names=list(regime=c('a', 'a.OU'), xy=NULL))
-Sigma <- abind::abind(a.Sigma, a.Sigma, along=-1, new.names=list(regime=c('a','a.OU'), x=NULL, y=NULL))
-Sigmae <- abind::abind(a.Sigmae2, a.Sigmae2, along=-1, new.names=list(regime=c('a','a.OU'), x=NULL, y=NULL))
-
-#Sigma <- abind::abind(a.Sigma, b.Sigma, along=-1, new.names=list(regime=c('a','b'), x=NULL, y=NULL))
-#Sigmae <- abind::abind(a.Sigmae2, b.Sigmae2, along=-1, new.names=list(regime=c('a','b'), x=NULL, y=NULL))
+Sigma <- abind::abind(a.Sigma, b.Sigma, along=-1, new.names=list(regime=c('a','b'), x=NULL, y=NULL))
+Sigmae <- abind::abind(a.Sigmae2, b.Sigmae2, along=-1, new.names=list(regime=c('a','b'), x=NULL, y=NULL))
 
 # regime 'a', traits 1, 2 and 3
-
-# regime 'a', trait 1
-model.a.1 <- list(X0 = a.X0[1],
-                  Sigma=Sigma['a',1,1,drop=FALSE],
-                  Sigmae=Sigmae['a',1,1,drop=FALSE])
-class(model.a.1) <- 'BM'
-
-model.a.1.OU <- list(X0 = a.X0[1],
-                  Alpha=Alpha['a.OU',1,1,drop=FALSE],
-                  Theta=Theta['a.OU',1,drop=FALSE],
-                  Sigma=Sigma['a.OU',1,1,drop=FALSE],
-                  Sigmae=Sigmae['a.OU',1,1,drop=FALSE])
-class(model.a.1.OU) <- 'OU'
-
 
 model.a.123 <- list(X0 = a.X0,
                     Sigma=Sigma['a',,,drop=FALSE],
@@ -84,35 +59,10 @@ class(model.a.123) <- 'BM'
 
 ################ 1st Validation ######################################################
 
-
-
-########
-N <- 20
-
-# tree with one regime
-tree.a <- phytools::pbtree(n=N, scale=1)
-
-traits.a.1 <- mvsim(tree.a, model.a.1, 0, verbose=TRUE)
-
-mvlik(traits.a.1$values + traits.a.1$errors, tree.a,model.a.1)
-
-mvlik(traits.a.1$values + traits.a.1$errors, tree.a,model.a.1.OU)
-
-POUMM::likPOUMMGivenTreeVTips(
-  z = traits.a.1$values[,1] + traits.a.1$errors[,1],
-  tree = tree.a,
-  0,
-  0,
-  sigma = sqrt(model.a.1$Sigma[1,1,1]),
-  sigmae = sqrt(model.a.1$Sigmae[1,1,1]),
-  g0 = a.X0[1])
-
-#######
-
 context(ctx <- "R=1/k=3/N=2")
 
 # number of tips
-N <- 20
+N <- 2
 
 # tree with one regime
 tree.a <- phytools::pbtree(n=N, scale=1)
@@ -164,7 +114,7 @@ test_that(paste(ctx, "Match multivariate likelihood of independent traits regime
 context(ctx <- "R=1/k=3/N=400")
 
 # number of tips
-N <- 100
+N <- 400
 
 tree.a <- phytools::pbtree(n=N, scale=1)
 
@@ -216,15 +166,15 @@ test_that(paste(ctx, "Match multivariate likelihood of independent traits regime
 
 ################ 2nd Validation ######################################################
 
-EPS <- 10^-8
+#EPS <- 10^-8
 
-b.X0 <- c(12, 4, 3)
-b.Sigma <- rbind(c(1.6, .3, .3),
-                 c(.3, 0.3, .4),
-                 c(.3, .4, 2))
-b.Sigmae2 <- rbind(c(0, 0, 0),
-                   c(0, 0, 0),
-                   c(0, 0, 0))
+#b.X0 <- c(12, 4, 3)
+#b.Sigma <- rbind(c(1.6, .3, .3),
+#                 c(.3, 0.3, .4),
+#                 c(.3, .4, 2))
+#b.Sigmae2 <- rbind(c(0, 0, 0),
+#                   c(0, 0, 0),
+#                   c(0, 0, 0))
 
 b.OU.Alpha <- rbind(c(EPS, 0, 0),
                     c(0, EPS, 0),
@@ -251,7 +201,7 @@ class(model.b.123.OU) <- 'OU'
 
 context(ctx <- "R=1/k=3/N=2")
 
-N=10
+N=400
 
 tree.b <- phytools::pbtree(n=N, scale=1)
 
