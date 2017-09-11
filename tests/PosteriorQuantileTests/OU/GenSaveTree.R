8b55eb9b7382511a2087476ad491a76dd1248ad2
gasimo <georgios.asimomitis@gmail.com>
2017-09-11 16:58:00 +0200

    R scripts for Posterior Quantiles - Cluster Version

---
+++ tests/PosteriorQuantileTests/OU/GenSaveTree.R
@@ -0,0 +1,4 @@
+N=200
+tree <- phytools::pbtree(n=N, scale=1)
+save(tree, file="Tree.RData")
+
--- tests/PosteriorQuantileTests/OU/runReplication.R
+++ tests/PosteriorQuantileTests/OU/runReplication.R
@@ -1,67 +1,62 @@
-source("postQuantileTools.R")
+source('postQuantileTools.R')
+library(adaptMCMC)
+library(PCMBase)
 
 #1. first read from the command line arguments the replication id (an integer number)
 
+args<-commandArgs(TRUE)
+
+replication = 0
+# test if there is exactly one argument: if not, return an error
+
+if (length(args)!=1) {
+  stop("One argument must be supplied! It is the replication id", call.=FALSE)
+
+} else {
+  # default output file
+  replication = as.numeric(args[1])
+}
 
 # 2. use this integer to set the random generator seed (set.seed())
 
+set.seed(replication)
 
 # 3. read the tree into a global variable
 
+tree = get(load("Tree.RData"))
+
 # 4. param.replicates <- generate.seeds(replications) (just one replication!)
 
+param.replicates <- generate.seeds(1)
 
 # 5. go on with the replication
+
+Alpha = param.replicates[['Alpha']][[1]]   ################## remember to put iter
+Theta = param.replicates[['Theta']][[1]]
+Sigma = param.replicates[['Sigma']][[1]]
+Sigmae = param.replicates[['Sigmae']][[1]]
+
+param.m = list('Alpha' = Alpha,'Theta'= Theta,'Sigma' = Sigma,'Sigmae'=Sigmae)  ######### or I take out the names
+
+model = generate.model(param.m)
+
+data = generate.data(tree,model)
+traits = data$traits
+
+param.l = c(Alpha[1,1],Alpha[2,2],Theta,Sigma[1,1],Sigma[1,2],Sigma[2,2],Sigmae[1,1],Sigmae[2,2])
+
+samples_res = generate.samples(posterior,300000,tree,traits)
+
+samp.coda <- convert.to.coda(samples_res)
+
+post.quant = compute.postquant(samples_res$samples,param.l)
+
 
 # ... after executing the MCMC
 
 # 6. to store the resulting posterior quantile vector in a file job_replication#id.RData
 
-
+save(post.quant, file=paste("job_replication",replication,".RData",sep=""))
+save(samp.coda, file=paste("coda_sample_job_replication",replication,".RData",sep=""))
 # 7. once this is tested locally, the next task is to copy everything to the cluster euler:
 # 7.1. copy and install the package PCMBase on the cluster
-
-
-
-
-  Alpha = param.replicates[['Alpha']][[iter]]   ################## remember to put iter
-  Theta = param.replicates[['Theta']][[iter]]
-  Sigma = param.replicates[['Sigma']][[iter]]
-  Sigmae = param.replicates[['Sigmae']][[iter]]
-
-  param.m = list('Alpha' = Alpha,'Theta'= Theta,'Sigma' = Sigma,'Sigmae'=Sigmae)  ######### or I take out the names
-
-  model = generate.model(param.m)
-
-  if (iter==1){
-    nodes = 100
-    data = generate.data(nodes,model)
-    tree = data$tree
-    traits = data$traits
-  }
-
-
-  param.l = c(Alpha[1,1],Alpha[2,2],Theta,Sigma[1,1],Sigma[1,2],Sigma[2,2],Sigmae[1,1],Sigmae[2,2])
-
-  samples_res[[iter]] = generate.samples(posterior,10,tree,traits)
-
-  post.quant[[iter]] = compute.postquant(samples_res[[iter]]$samples,param.l)
-
-
-
-param.names = c('Alpha11','Alpha22','Theta1','Theta2','Sigma11','Sigma12','Sigma22','Sigmae11','Sigmae22')
-num.param = length(param.l)
-
-x = list()
-length(x) = num.param
-
-for (j in 1:num.param){
-
-  x[[j]] = sapply(1:replications, function(i) (c(post.quant[[i]][j])))
-  filename = paste(param.names[j],"histOU.png")
-  png(filename)
-  hist(x[[j]], main=paste("Histogram for OU parameter",param.names[j]),xlab = param.names[j]
-       ,border="black",col="blue",las=1, breaks=5)
-  dev.off()
-}
-
